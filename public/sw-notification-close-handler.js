1fb4e775fa61fff8c15b0818109c9904204c8349
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-02-18 20:44:28 -0600

    feat: backup

--- public/sw-notification-close-handler.js
+++ public/sw-notification-close-handler.js
@@ -3,16 +3,29 @@ self.handleNotificationClose = (event) => {
     self.cancelNotificationExpiration(event.notification.tag);
   }
 
-  const data = event.notification.data;
+  const metadata = event.notification.data || {};
 
   const closedAt = Date.now();
 
-  const { visibleAt } = event.notification.data;
+  const { visibleAt } = metadata;
 
   const visibleTimeMs = closedAt - visibleAt;
   const visibleTimeSec = Math.floor(visibleTimeMs / 1000);
 
   console.log({ closeInSec: visibleTimeSec });
 
-  console.log("Notificación cerrada por el usuario ❌", data);
+  console.log("Notificación cerrada por el usuario ❌", metadata);
+
+  const payloadMetric = {
+    ...metadata,
+    reactionTimeSec: visibleTimeSec,
+    accionAt: new Date(accionAt).toISOString(),
+  };
+
+  event.waitUntil(
+    self.notifyMetric({
+      ...payloadMetric,
+      eventType: "NOTIFICATION_CLOSED",
+    }),
+  );
 };
