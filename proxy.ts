1d9498f42f15290570ba72037665aa555141a142
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-02-19 15:07:34 -0600

    feat: backup

--- proxy.ts
+++ proxy.ts
@@ -3,9 +3,7 @@ import { NextResponse } from "next/server";
 
 import { headersDefault } from "./common/axiosClient";
 
-//let idx = 0;
-
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const url = request.nextUrl;
   const pathname = url.pathname;
   const token = request.cookies.get("token")?.value ?? "";
@@ -21,11 +19,7 @@ export async function middleware(request: NextRequest) {
   // token valido = 0
   // token invalido = 1
 
-  //idx++;
-
   const fetchUrl = process.env.NEXT_PUBLIC_API_URL + "/auth/check-token";
-
-  //console.log({ idx, url: request.url, fetchUrl });
 
   const data = await (
     await fetch(fetchUrl, {
@@ -41,29 +35,25 @@ export async function middleware(request: NextRequest) {
 
   // Token valido
   if (statusCode === 0) {
-    //console.log("token valido ************");
-
     if (pathname === "/") {
       return NextResponse.redirect(new URL("/dashboard", request.url));
     }
     return NextResponse.next();
-  } else {
-    //console.log("token invalido ************");
-
-    const searchParams = url.searchParams;
-    if (searchParams.has("token")) {
-      searchParams.delete("token");
-      url.search = searchParams.toString();
-      return NextResponse.redirect(new URL(url.pathname, request.url));
-    }
-
-    // Token invalido
-    if (pathname === "/") {
-      return NextResponse.next();
-    }
-
-    return NextResponse.redirect(new URL("/", request.url));
   }
+
+  const searchParams = url.searchParams;
+  if (searchParams.has("token")) {
+    searchParams.delete("token");
+    url.search = searchParams.toString();
+    return NextResponse.redirect(new URL(url.pathname, request.url));
+  }
+
+  // Token invalido
+  if (pathname === "/") {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
 }
 
 export const config = {
