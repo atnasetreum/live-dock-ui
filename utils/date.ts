19a33ff8c4ea7666d7795e5a88a1daff05d64f83
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-02-18 22:36:37 -0600

    feat: backup

--- utils/date.ts
+++ utils/date.ts
@@ -1,5 +1,22 @@
-import { DateTime } from "luxon";
+/* import { DateTime } from "luxon";
 
 export const getCurrentDate = (): string => {
   return DateTime.now().toISODate() ?? "";
+  //return DateTime.now().setZone("America/Mexico_City").toISODate() ?? "";
+};
+ */
+
+const serverTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+console.log("Servidor:", serverTimezone);
+
+// O usando getTimezoneOffset para obtener la diferencia en minutos
+const offset = new Date().getTimezoneOffset();
+console.log("Diferencia en minutos:", offset);
+
+export const getCurrentDate = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
 };
