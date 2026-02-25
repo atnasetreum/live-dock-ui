05c0d77cbd5d41e8d4a64bfe5f7b19425d41d032
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-02-24 22:48:06 -0600

    feat: backup

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ importScripts("/sw-push-handler.js");
 importScripts("/sw-notification-click-handler.js");
 importScripts("/sw-notification-close-handler.js");
 
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
   console.log("[SW] Service Worker instalado");
   self.skipWaiting();
   event.waitUntil(
